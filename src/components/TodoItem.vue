c7ddc22e449e97f18101d19d584bb34fe222ca97
rohit8450 <kartiksarwan2017.comp@mmcoe.edu.in>
2023-12-03 10:59:14 +0530

    Adding editable form in Todo

--- src/components/TodoItem.vue
+++ src/components/TodoItem.vue
@@ -1,13 +1,32 @@
 <template>
-    <h1>Todo single</h1>
+    <li class="d-flex align-items-center list-group-item">
+      <button class="btn border-0 text-left flex-grow-1">{{todoString}}</button>
+      <form class="flex-grow-1">
+        <input type="text" class="form-control" />
+      </form>
+      <button class="btn btn-outline-primary">Edit</button>
+      <button class="btn btn-outline-danger">Delete</button>
+    </li>
 </template>
 
 <script>
 export default {
-   name: "TodoItem"
+   name: "TodoItem",
+   props: {
+    todoString: String,
+    completed: Boolean
+   },
+   data(){
+    return {
+        isEditing: false,
+        newTodoString: ""
+    }
+   }
 }
 </script>
 
 <style scoped>
-
+  .completed {
+    text-decoration: line-through;
+  }
 </style>
